a17eab4cc822a6f245a97cf87b0caabfc67d5388
Taiwan9 <clownser1994@gmail.com>
2023-05-13 23:24:52 -0300

    SistemeFinanceiro

---
+++ src/components/data/constants/usuarioFalso.ts
@@ -0,0 +1,11 @@
+
+
+import Id from "@/logic/core/comum/Id";
+import Usuario from "@/logic/core/usuario/Usuario";
+
+export default {
+    id: Id.novo(),
+    nome:'Taiwan Marinho',
+    email:'Taiwan-123@xmail.com',
+    imagemUrl:null,
+} as Usuario
---
+++ src/logic/core/usuario/Usuario.ts
@@ -0,0 +1,8 @@
+export default interface Usuario{
+    id:string
+    nome:string
+    email:string
+    cpf?:string
+    telefone?:string
+    imagemUrl:string | null
+}
